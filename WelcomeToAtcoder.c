4f406846349b682a6bb0c9e9d49501b00bb06b41
ChocoLe-io <72448356+ChocoLe-io@users.noreply.github.com>
2021-06-07 09:31:49 +0900

    Add files via upload

---
+++ WelcomeToAtcoder.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a, b, c;
+	char s[100];
+
+	scanf("%d", &a);
+
+	scanf("%d%d", &b, &c);
+
+	scanf("%s", &s);
+
+	printf("%d %s", a + b + c, s);
+
+	return 0;
+}
